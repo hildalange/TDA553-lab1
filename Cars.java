fd8fa935d77fc726923037415202372349f84d23
hildalange <hilda.lange@gmail.com>
2022-11-11 10:08:30 +0100

    vet ej om detta funkar, fick hjälp

--- Cars.java
+++ Cars.java
@@ -6,7 +6,16 @@ public class Cars implements Movable{
     protected double currentSpeed;
     private double enginePower;
     private Color color;
-
+    private double x;
+    private double y;
+    public enum Direction {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST;
+    }
+    private Direction currentDirection;
+    
 
     public Cars(String modelName, int nrDoors, double currentSpeed, double enginePower, Color color){
         this.modelName = modelName;
@@ -14,16 +23,25 @@ public class Cars implements Movable{
         this.currentSpeed = currentSpeed;
         this.enginePower = enginePower;
         this.color = color;
+        this.x = 0;
+        this.y = 0;
+        this.currentDirection = Direction.NORTH;
     }
+
     public int getNrDoors(){
-        return nrDoors;
+        return this.nrDoors;
     }
+
     public double getEnginePower(){
-        return enginePower;
+        return this.enginePower;
     }
 
     public double getCurrentSpeed(){
         return currentSpeed;
+    }
+
+    public void setCurrentSpeed(double speed){
+        currentSpeed = speed;
     }
 
     public Color getColor(){
@@ -41,15 +59,49 @@ public class Cars implements Movable{
     public void stopEngine(){
 	    currentSpeed = 0;
     }
-    public void move(){
 
+    public void move(){
+        switch (this.currentDirection) {
+            case NORTH: {
+                y += currentSpeed;
+            }
+            case EAST: {
+                x -= currentSpeed;
+            }
+            case SOUTH: {
+                y -= currentSpeed;
+            }
+            case WEST: 
+            default: {
+                x += currentSpeed;
+            }
+        }
     }
+
     public void turnLeft(){
         
     }
-    @Override
+
     public void turnRight() {
-        // TODO Auto-generated method stub
         
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    
+
+    
 }
