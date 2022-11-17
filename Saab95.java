c004e73855fb93d22e16b1143eb6ee8ae5512665
hildalange <hilda.lange@gmail.com>
2022-11-17 13:27:45 +0100

    hej

--- Saab95.java
+++ Saab95.java
@@ -5,7 +5,7 @@ public class Saab95 extends Cars{
     public boolean turboOn;
     
     public Saab95(){
-        super("Saab95", 2, 0, 125, Color.red);
+        super("Saab95", 2, 0, 125, Color.red, x, y);
 	    turboOn = false;
         stopEngine();
     }
